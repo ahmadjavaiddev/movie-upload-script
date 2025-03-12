44c92f0b8ff1c8d0c3a462660ea1e72aa1b893d7
Ahmad Javaid Dev <ahmadjavaiddev@gmail.com>
2025-03-12 22:55:27 +0500

    Add Puppeteer launch argument to disable /dev/shm usage

--- browser.js
+++ browser.js
@@ -35,7 +35,11 @@ connectDB()
   .then(async () => {
     const browser = await puppeteer.launch({
       headless: true,
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disable-dev-shm-usage",
+      ],
       defaultViewport: null,
       dumpio: true,
     });
